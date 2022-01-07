5f9f2aa50d72b35820911f46ed336f9e9314ee9e
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-07 23:05:21 +0100

    V1 07.01.2022

--- validators.py
+++ validators.py
@@ -11,13 +11,36 @@ def input_postal_code(prompt, *postalcodes):
 
 
 def _check_postalcode(value, postalcodes):
-
     try:
         number = int(value)
     except ValueError:
         raise ValueError(f"{value} is not numerical! Only numbers, please!")
 
     if number not in postalcodes:
-        raise ValueError(f"Postal code {number} is not valid! Valid codes are: {','.join([str(value) for value in postalcodes])}")
+        raise ValueError(
+            f"Postal code {number} is not valid! Valid codes are: {','.join([str(value) for value in postalcodes])}")
 
     return number
+
+
+def input_bounded_integer(prompt, description, minimum, maximum):
+    while True:
+        try:
+            input_value = int(input(f"{prompt}{description}! (Enter \'q\' to quit): "))
+            if (not isinstance(input_value, int)) and (input_value.upper() == "Q"):
+                    return "Good bye!"
+            else:
+                return _check_bounded_integer(input_value, description, minimum, maximum)
+
+        except ValueError as error:
+            print(error)
+
+
+def _check_bounded_integer(value, description, minimum, maximum):
+    if not isinstance(value, int):
+        raise ValueError(f" Your input {value} for {description} was not numerical!")
+
+    if not minimum <= value <= maximum:
+        raise ValueError(f"Your input {value} for {description} is not between  {minimum} and {maximum}!")
+
+    return value
