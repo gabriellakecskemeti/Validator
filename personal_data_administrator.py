0d94bd241d21394a343debd0c6e0b93032930825
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-13 21:03:58 +0100

    cleaning out warnings.

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -4,31 +4,27 @@ import validators
 #                                   "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
 
 while True:
-        selection = input(
-            "Please choose which aspect to edit: (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
+    selection = input(
+        "Please choose which aspect to edit: (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
 
-        match selection:
-            case "DAY OF BIRTH" | "DAY" | "D":
-                print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
-                break
-            case "MONTH OF BIRTH" | "MONTH" | "M":
-                print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
-                break
-            case "YEAR OF BIRTH" | "YEAR" | "Y":
-                print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
-                break
-            case "POSTAL CODE" | "POSTAL" | "P":
-                print(validators.input_postal_code("Please enter your postal "
-                                          "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
-                break
-            case "QUIT" | "Q":
+    match selection:
+        case "DAY OF BIRTH" | "DAY" | "D":
+            print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
+            break
+        case "MONTH OF BIRTH" | "MONTH" | "M":
+            print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
+            break
+        case "YEAR OF BIRTH" | "YEAR" | "Y":
+            print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
+            break
+        case "POSTAL CODE" | "POSTAL" | "P":
+            print(validators.input_postal_code("Please enter your postal "
+                                               "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
+            break
+        case "QUIT" | "Q":
 
-                break
-            case _:
-                print("Not valid option! Please try it again!")
-
-
+            break
+        case _:
+            print("Not valid option! Please try it again!")
 
 print("Thank you for using the Validator!")
-
-
