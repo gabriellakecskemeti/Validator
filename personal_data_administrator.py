0b53fb3d7dba3a45cefb0095bf2a3d84f158d6ae
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-20 21:56:50 +0100

    what is new: -comments -take off utf-8 -check if the file exist and send messages depending on this -if the file exist, the program append the next item.

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -1,30 +1,34 @@
 import validators
 
-# print(validators.input_postal_code("Please enter your postal "
-#                                   "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
+person = {"Name": None, "Address": None, "Postal": None, "Day": None, "Month": None, "Year": None}
 
 while True:
     selection = input(
-        "Please choose which aspect to edit: (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
+        "Please choose which aspect to edit: (n)ame, (a)ddress, (p)ostal code, (d)ay of birth, (m)onth of birth, (y)ear of birth (or enter 'q' to quit):").upper()
 
     match selection:
-        case "DAY OF BIRTH" | "DAY" | "D":
-            print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
-            break
-        case "MONTH OF BIRTH" | "MONTH" | "M":
-            print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
-            break
-        case "YEAR OF BIRTH" | "YEAR" | "Y":
-            print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
-            break
+        case "NAME" | "N":
+            person["Name"] = validators.input_string("Please Enter Name, (Enter 'q' to quit): ")
+        case "ADDRESS" | "A":
+            person["Address"] =validators.input_string("Please Enter your Address, (Enter 'q' to quit): ")
         case "POSTAL CODE" | "POSTAL" | "P":
-            print(validators.input_postal_code("Please enter your postal "
-                                               "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
-            break
-        case "QUIT" | "Q":
+            person["Postal"] = validators.input_postal_code("Please enter your postal "
+                                                                  "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040,
+                                                                  1050)
+        case "DAY OF BIRTH" | "DAY" | "D":
+            person["Day"] = validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31)
+        case "MONTH OF BIRTH" | "MONTH" | "M":
+            person["Month"] = validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12)
+        case "YEAR OF BIRTH" | "YEAR" | "Y":
+            person["Year"] = validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022)
 
+        case "QUIT" | "Q":
             break
         case _:
             print("Not valid option! Please try it again!")
 
+#validators.dict_to_csv(person, "c:people.csv")
+validators.dict_to_csv(person, "people.csv")
+
+print(person)
 print("Thank you for using the Validator!")
--- validators.py
+++ validators.py
@@ -1,5 +1,14 @@
+import csv
+from os.path import isfile
+
+
 def input_postal_code(prompt, *postalcodes):
-    number = 0
+    """Postal code entry
+
+    :param prompt:this a text to instruct the user what is possible to enter and how to quit.
+    :param postalcodes:a list of postal codes to accept
+    :return: the selected postal code
+    """
     while True:
         number_char = input(prompt)
         if number_char.upper() == "Q":
@@ -11,6 +20,14 @@ def input_postal_code(prompt, *postalcodes):
 
 
 def _check_postalcode(value, postalcodes):
+    """
+    method to check the entered postal code if it is part of the accepted codes
+    list and if the entered postal code is numeric.
+    :param value: entered postal code
+    :param postalcodes: list of accepted codes
+    :return: checked postal code in string format or error message
+    if the code is not numeric or if the code is not in the list of postalcodes.
+    """
     try:
         number = int(value)
     except ValueError:
@@ -20,10 +37,18 @@ def _check_postalcode(value, postalcodes):
         raise ValueError(
             f"Postal code {number} is not valid! Valid codes are: {','.join([str(value) for value in postalcodes])}")
 
-    return number
+    return str(number)
 
 
 def input_bounded_integer(prompt, description, minimum, maximum):
+    """
+    method to enter a bounded integer.
+    :param prompt: text instruction for the user what it is possible to enter.
+    :param description: the name of the integer. For example month or year etc.
+    :param minimum: smallest accepted value of the integer
+    :param maximum: highest accepted value of the integer
+    :return: checked integer value or None if the user wants to quit.
+    """
     while True:
         try:
             input_value = input(f"{prompt}{description}! (Enter \'q\' to quit): ")
@@ -37,8 +62,17 @@ def input_bounded_integer(prompt, description, minimum, maximum):
 
 
 def _check_bounded_integer(value, description, minimum, maximum):
+    """
+    method to check a bounded integer
+    :param value: the value what we want to check
+    :param description: the name of this value. For example day, month or year, etc.
+    :param minimum: smallest accepted value of the integer
+    :param maximum: highest accepted value of the integer
+    :return: checked integer value or an error if the value is not integer
+    or not between the defined bounds.
+    """
     try:
-        integer_value=int(value)
+        integer_value = int(value)
     except ValueError:
         raise ValueError(f" Your input {value} for {description} was not numerical!")
 
@@ -46,3 +80,56 @@ def _check_bounded_integer(value, description, minimum, maximum):
         raise ValueError(f"Your input {value} for {description} is not between  {minimum} and {maximum}!")
 
     return value
+
+
+def input_string(prompt):
+    """
+    method to input any string value
+    :param prompt: text for the user, to explain what should be entered and how to quit.
+    :return: the entered value or None if the user wants to quit.
+    """
+    input_value = input(prompt)
+    if input_value.upper() == "Q":
+        return
+    return input_value
+
+
+def dict_to_csv(mydict, myfile):
+    """
+    method to write a dictionary to a csv file
+    :param mydict: the dictionary
+    :param myfile: the path and file name
+    :return:
+    """
+    answer = ""
+
+    while answer.upper() != "Q":
+        try:
+            a_w_parameter = "w"
+
+            if isfile(myfile):
+                a_w_parameter = "a"
+
+            with open(myfile, a_w_parameter, newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+                #the writer method works only right for special characters if i take off the utf-8 parameter.
+
+                #writer = csv.DictWriter(csvfile, fieldnames=mydict.keys())
+                if a_w_parameter == "w":
+                    writer.writerow(mydict.keys())
+
+
+                new_row = list()
+                for i in mydict.keys():
+                    new_row.append(mydict[i])
+                writer.writerow(new_row)
+            answer = "Q"
+        except PermissionError:
+            if a_w_parameter == "a":
+                print("\ncsv file open! Please close the file or contact the system administrator!")
+                answer = input("\nPlease press Enter if you closed the file or (Q) to quit! : ")
+            else:
+                print("\nPlease check the path, you do not have permission to write there."
+                      " \nPlease contact the system administrator!")
+                answer = "Q"
+    return
